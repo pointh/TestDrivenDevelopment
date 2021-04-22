18ed089b13c6643e021432a93a0bde443b413edd
Pavel Svec <pavel.svec@itcrawl.cz>
2021-04-22 15:27:38 +0200

    Add project files.

---
+++ TestDrivenDevelopment/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestDrivenDevelopment
+{
+    public class Delitelnost
+    {
+        public static string VypisDelitele(int n)
+        {
+            string s = string.Empty;
+
+            for(int i = 1; i < n; i++)
+            {
+                if (n % i == 0)
+                    s += i.ToString();
+            }
+
+            return s;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestDrivenDevelopment;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void Dvanact_Delitele_Vraci12346()
+        {
+            string r = string.Empty;
+
+            r = Delitelnost.VypisDelitele(12);
+
+            Assert.AreEqual(r, "12346");
+        }
+
+        [TestMethod]
+        public void Jedna_Delitele_VraciPrazdne()
+        {
+            string r = string.Empty;
+
+            r = Delitelnost.VypisDelitele(1);
+
+            Assert.AreEqual(r, string.Empty);
+        }
+
+        [TestMethod]
+        public void Sedmnast_Delitele_Vraci1()
+        {
+            string r = string.Empty;
+
+            r = Delitelnost.VypisDelitele(17);
+
+            Assert.AreEqual(r, "1");
+        }
+
+        [TestMethod]
+        public void Minus1_Delitele_VraciPrazdne()
+        {
+            string r = string.Empty;
+
+            r = Delitelnost.VypisDelitele(-1);
+
+            Assert.AreEqual(r, string.Empty);
+        }
+    }
+}
